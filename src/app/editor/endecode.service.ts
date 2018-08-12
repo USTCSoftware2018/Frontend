daa36f899d241bcfd1dfcc3a02aca22ae25f6191
andytt <936664530@qq.com>
2018-08-12 21:30:51 +0800

    初步完善编辑器文档第一版

--- src/app/editor/endecode.service.ts
+++ src/app/editor/endecode.service.ts
@@ -1,3 +1,10 @@
+// ============================================================================
+// endecode.service.ts
+// ----------
+// ertuil 2018.8.10
+// 该文件定义了与后端的结构，获取操作、过程以及文章。并且将xml转化为对象映射关系
+// ============================================================================
+
 import { Injectable } from '@angular/core';
 import {ExperimentArticle} from './types';
 
@@ -6,5 +13,25 @@ import {ExperimentArticle} from './types';
 })
 export class EndecodeService {
 
+  parser: any = new DOMParser(); // xml解析器
+
+  private _currentArticle: ExperimentArticle;
+
+  public get currentArticle(): ExperimentArticle {
+    return this._currentArticle;
+  }
+  public set currentArticle(value: ExperimentArticle) {
+    this._currentArticle = value;
+  }
+
   constructor() { }
+
+
+  encode(articleid: number): void {
+    // 将ORM转化为 xml 并发起保存
+  }
+
+  decode(articleid: number): void {
+    // 从后端获取xml 并转化为 ORM
+  }
 }
