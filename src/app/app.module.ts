b94c98266bb91e9d231a05dc9a42bff01c6c0c2b
andytt <936664530@qq.com>
2018-08-14 09:49:02 +0800

    路由框架 和 http错误框架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,22 +9,26 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import zh from '@angular/common/locales/zh';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './route/route.module';
+import { Error404Component } from './error/error404/error404.component';
 
 registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    AppRoutingModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/route/route.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { Error404Component } from '../error/error404/error404.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: '**', component: Error404Component}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
