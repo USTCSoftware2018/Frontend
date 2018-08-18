cc0f16b39080513663aa4d4afc8b339e25288f20
heliyang <596136894@qq.com>
2018-08-18 12:41:10 +0800

    new change

---
+++ src/app/profile/personal-information/personal-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-information',
+  templateUrl: './personal-information.component.html',
+  styleUrls: ['./personal-information.component.less']
+})
+export class PersonalInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
