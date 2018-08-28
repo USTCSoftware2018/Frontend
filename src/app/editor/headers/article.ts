773bf06b1c5dd7df3517835eb65ffd769a5e77d2
andytt <936664530@qq.com>
2018-08-28 21:03:08 +0800

    article

--- src/app/editor/headers/article.ts
+++ src/app/editor/headers/article.ts
@@ -1,6 +1,46 @@
+import {AppendixHeader} from './appendix';
+import { ArgumentOutOfRangeError } from 'rxjs';
 // 这里是文章的定义
 
 export class ArticleHeader {
-    title: string;
-    
+
+    // 元数据部分
+    title: string;  // 标题
+    author: string[];   // 作者
+    mdate: string;  // 修改时间
+    ndate: string;  // 创建时间
+    introduction: string;   // 介绍
+    result: AppendixHeader; // 结果部分
+    label: string[];    // 标签部分
+
+    // 节点部分
+
+    constructor () {}
+}
+
+// 节点父亲元素
+export class NodeHeader {
+    stepId: string; // 对应节点id
+    stepAppendix: AppendixHeader;
+
+    constructor (id?: string) {
+        this.stepId = id;
+        this.stepAppendix = new AppendixHeader();
+    }
+}
+
+export class StepNodeHeader extends NodeHeader {
+    fields: object;
+    constructor (id?: string) {
+        super(id);
+        this.fields = {};
+    }
+}
+
+export class ProcessNodeHeader extends NodeHeader {
+    fields: StepNodeHeader[];
+    constructor (id?: string) {
+        super(id);
+        this.fields = Array<StepNodeHeader>();
+    }
 }
