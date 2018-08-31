f0ea6baa0455e51805a95273574c6b47a4bd744a
andytt <936664530@qq.com>
2018-08-31 20:44:29 +0800

    1

--- src/app/route/route.module.ts
+++ src/app/route/route.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserSetModule } from '../user-set/user-set.module';
+//import { UserSetModule } from '../user-set/user-set.module';
 import { Error404Component } from '../error/error404/error404.component';
 import {ReportComponent} from '../report/report.component';
 import {MainComponent as EditorMainComponent} from '../editor/views/main/main.component';
@@ -8,7 +8,7 @@ import {MainComponent as EditorMainComponent} from '../editor/views/main/main.co
 const appRoutes: Routes = [
   { path: 'editor', component: EditorMainComponent},
   { path: 'report', component: ReportComponent},
-  { path: 'user-set',loadChildren: '../user-set/user-set.module#UserSetModule',data: { preload: true } },
+//  { path: 'user-set',loadChildren: '../user-set/user-set.module#UserSetModule',data: { preload: true } },
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: '**', component: Error404Component}
 ];
