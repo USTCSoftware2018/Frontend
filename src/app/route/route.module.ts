39bf71002f9c9604e9c73b266461403ae3d4e0ec
yjw1029 <1055863721@qq.com>
2018-09-03 19:33:29 +0800

    add router of signin

--- src/app/route/route.module.ts
+++ src/app/route/route.module.ts
@@ -6,15 +6,16 @@ import { ReportshowComponent} from '../reportshow/reportshow.component';
 import {MainComponent as EditorMainComponent} from '../editor/views/main/main.component';
 import { ProfilePageComponent } from '../profile/profile-page/profile-page.component';
 import {UserinfoComponent} from '../userinfo/userinfo.component';
+import {SigninComponent} from '../signin/signin.component';
 
 const appRoutes: Routes = [
   { path: 'editor', component: EditorMainComponent},
   { path: 'reportshow', component: ReportshowComponent},
-  { path: 'userset', loadChildren: '../user-set/user-set.module#UserSetModule',data: { preload: true } },
+  { path: 'userset', loadChildren: '../user-set/user-set.module#UserSetModule', data: { preload: true } },
   { path: 'profile', component: ProfilePageComponent},
-  { path: 'user-set', loadChildren: '../user-set/user-set.module#UserSetModule', data: { preload: true } },
   { path: 'userinfo', component: UserinfoComponent},
   { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent},
   { path: '**', component: Error404Component},
 ];
 
