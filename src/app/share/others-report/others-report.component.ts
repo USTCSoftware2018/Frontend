3d1e517c766c3194ecd694de57af5002c613b422
yjw1029 <1055863721@qq.com>
2018-09-06 17:20:49 +0800

    add zorro to share

---
+++ src/app/share/others-report/others-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-others-report',
+  templateUrl: './others-report.component.html',
+  styleUrls: ['./others-report.component.less']
+})
+export class OthersReportComponent implements OnInit {
+  report: {};
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
