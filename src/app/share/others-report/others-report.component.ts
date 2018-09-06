327d2f2a1cc4c0c440b72b232bf7a65eab2935e7
yjw1029 <1055863721@qq.com>
2018-09-06 17:56:58 +0800

    panda z-index

--- src/app/share/others-report/others-report.component.ts
+++ src/app/share/others-report/others-report.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Report } from './others-report';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-others-report',
   templateUrl: './others-report.component.html',
-  styleUrls: ['./others-report.component.less']
+  styleUrls: ['./others-report.component.less'],
+  animations: [
+    trigger('shrinkOut', [
+      state('in', style({height: 0})),
+      transition('void => *', [
+        style({height: 0}),
+        animate(250, style({height: '*'}))
+      ])
+    ])
+  ]
 })
 export class OthersReportComponent implements OnInit {
-  report: {};
+  @Input() report: Report;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/share/others-report/others-report.ts
@@ -0,0 +1,7 @@
+export class Report {
+  title: string;
+  overview: string;
+  comments: any;
+  likes: any;
+  topics: any;
+}
