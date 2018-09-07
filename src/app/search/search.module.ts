0f31868cc64fbb2011d0ec3af4087c151fb0b874
pyxaws <xk98@mail.ustc.edu.cn>
2018-09-07 11:30:06 +0800

    searchbox

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ShareModule } from '../share/share.module';
+import { NgZorroAntdModule, NZ_I18N, en_US, zh_CN } from 'ng-zorro-antd';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchboxComponent } from './searchbox/searchbox.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SearchRoutingModule
+    SearchRoutingModule,
+    ShareModule,
+    NgZorroAntdModule,
   ],
-  declarations: [SearchboxComponent]
+  declarations: [SearchboxComponent],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
 })
 export class SearchModule { }
