ad433dc7f61dec211816c9cf50701fc9de75db05
yjw1029 <1055863721@qq.com>
2018-09-17 22:07:04 +0800

    http signup/signin

--- src/app/authentic/signin/signin.component.ts
+++ src/app/authentic/signin/signin.component.ts
@@ -6,6 +6,9 @@ import {
   Validators,
 } from '@angular/forms';
 import {HttpService} from '../../http.service';
+import {ApiResult} from '../../Interface/ApiResult';
+import {NzMessageService} from 'ng-zorro-antd';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -15,7 +18,9 @@ import {HttpService} from '../../http.service';
 export class SigninComponent implements OnInit {
   validateForm: FormGroup;
   shake = false;
-  constructor(private http: HttpService) { }
+  constructor(private http: HttpService,
+              private message: NzMessageService,
+              private router: Router) { }
   ngOnInit(): void {
     this.validateForm = new FormGroup({
       'username': new FormControl(null,
@@ -27,9 +32,20 @@ export class SigninComponent implements OnInit {
     });
   }
   submitForm(): void {
+    const signinInfo = this.validateForm.value;
     for (const i in this.validateForm.controls) {
-      this.validateForm.controls[ i ].markAsDirty();
-      this.validateForm.controls[ i ].updateValueAndValidity();
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+    this.http.user_login(signinInfo.username, signinInfo.password, this.judgeSignin);
+  }
+  judgeSignin = (result: ApiResult) => {
+    console.log(result);
+    if (result.success) {
+      this.message.success('Sign up sucessfully');
+      this.router.navigateByUrl('/explore');
+    } else {
+      this.message.error('Fail to Sign up.' + result.data.detail);
     }
   }
   get username() { return this.validateForm.get('username'); }
--- src/app/authentic/signup/signup.component.ts
+++ src/app/authentic/signup/signup.component.ts
@@ -6,6 +6,10 @@ import {
 } from '@angular/forms';
 import {forbiddenEmailValidator, forbiddenUsernameValidator, forbiddenAlphaValidator,
   forbiddenNumericValidator, forbiddenSpecialValidator} from './forbidden-signup';
+import {HttpService} from '../../http.service';
+import {NzMessageService} from 'ng-zorro-antd';
+import {ApiResult} from '../../Interface/ApiResult';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -15,7 +19,9 @@ import {forbiddenEmailValidator, forbiddenUsernameValidator, forbiddenAlphaValid
 export class SignupComponent implements OnInit {
   validateForm: FormGroup;
   shake = false;
-  constructor() { }
+  constructor(private http: HttpService,
+              private message: NzMessageService,
+              private router: Router) {}
 
   ngOnInit() {
     this.validateForm = new FormGroup({
@@ -40,11 +46,22 @@ export class SignupComponent implements OnInit {
     });
   }
   submitForm(): void {
+    const reginfo = this.validateForm.value;
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[ i ].markAsDirty();
       this.validateForm.controls[ i ].updateValueAndValidity();
     }
+    this.http.user_register(reginfo['username'], reginfo['username'], reginfo['email'], this.judgeRegister);
   }
+  judgeRegister(result: ApiResult): void {
+    console.log(result);
+    if (result.success) {
+      this.message.success('Sign up sucessfully');
+      this.router.navigateByUrl('/explore');
+    } else {
+      this.message.error('Fail to Sign up');
+    }
+  };
   get username() { return this.validateForm.get('username'); }
   get password() { return this.validateForm.get('password'); }
   get email() { return this.validateForm.get('email'); }
