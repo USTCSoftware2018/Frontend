6294c770debcdaf7b609665e2762dcf73e57bf60
pyxaws <xk98@mail.ustc.edu.cn>
2018-09-22 22:09:30 +0800

    correct

--- src/app/core/notification/notification.component.ts
+++ src/app/core/notification/notification.component.ts
@@ -4,8 +4,6 @@ import { Otheruser, Like } from '../../share/likereport/likereport';
 import { user1,user2,report1 } from '../../Interface/mock-user';
 import { NOTIS } from './mock-notification';
 import { LikeNotification, FollowNotification } from './notification';
-import { HttpService } from '../../http.service';
-import { ApiResult } from '../../Interface/ApiResult';
 
 @Component({
   selector: 'app-notification',
@@ -17,11 +15,7 @@ export class NotificationComponent implements OnInit {
   user = user1;
   notifications = NOTIS;
   isFollowing = true;
-  constructor(private http:HttpService) { }
-  callback = function(result:ApiResult){
-    console.log(result);
-  }
+  constructor() { }
   ngOnInit() {
-    this.http.get_myself(this.callback);
   }
 }
