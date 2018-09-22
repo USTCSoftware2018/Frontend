6f252df6f6f0ceb22da8993c6d0d026275b35066
yjw1029 <1055863721@qq.com>
2018-09-22 14:46:16 +0800

    2333

--- src/app/my-personal-center/my-personal-center-routing.module.ts
+++ src/app/my-personal-center/my-personal-center-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MyCenterIndexComponent} from './my-center-index/my-center-index.component';
+import {ReportAllComponent} from './user-set/report-all/report-all.component';
+import {ReportDraftComponent} from './user-set/report-draft/report-draft.component';
+import {UserReportComponent} from './user-set/user-report/user-report.component';
+import {UserNavComponent} from './user-set/user-nav/user-nav.component';
+import {UserBasicComponent} from './user-set/user-basic/user-basic.component';
+import {UserAccountComponent} from './user-set/user-account/user-account.component';
 
 const routes: Routes = [
   { path: '', component: MyCenterIndexComponent},
-  { path: 'userset', loadChildren: './user-set/user-set.module#UserSetModule'},
+  {
+    path: 'userset',
+    component: UserNavComponent,
+    children: [{
+      path: '',
+      redirectTo: 'user-basic',
+      pathMatch: 'full',
+    },
+      {
+        path: 'user-basic',
+        component: UserBasicComponent
+      },
+      {
+        path: 'user-account',
+        component: UserAccountComponent
+      },
+      {
+        path: 'user-report',
+        component: UserReportComponent,
+        children: [{
+          path: '',
+          redirectTo: 'report-all',
+          pathMatch: 'full'
+        },
+          {
+            path: 'report-all',
+            component: ReportAllComponent
+          },
+          {
+            path: 'report-draft',
+            component: ReportDraftComponent
+          }]
+      }]
+  }
 ];
 
 @NgModule({
--- src/app/my-personal-center/my-personal-center.module.ts
+++ src/app/my-personal-center/my-personal-center.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MyPersonalCenterRoutingModule } from './my-personal-center-routing.module';
 import { MyCenterIndexComponent } from './my-center-index/my-center-index.component';
 import { ShareModule } from '../share/share.module';
+import { UserSetModule } from './user-set/user-set.module';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import {en_US, NZ_I18N} from 'ng-zorro-antd';
@@ -13,7 +14,8 @@ registerLocaleData(en);
   imports: [
     CommonModule,
     MyPersonalCenterRoutingModule,
-    ShareModule
+    ShareModule,
+    UserSetModule,
   ],
   declarations: [MyCenterIndexComponent],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
