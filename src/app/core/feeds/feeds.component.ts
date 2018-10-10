40c72b5d2b77f1a903ce7ec57826e48b912fe1dd
yjw1029 <1055863721@qq.com>
2018-10-10 13:52:50 +0800

    233

--- src/app/core/feeds/feeds.component.ts
+++ src/app/core/feeds/feeds.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FEEDS } from './mock-feeds';
 import { LikeFeed, UploadFeed, FollowFeed } from './feeds';
+import {HttpService} from '../../http.service';
+import {ApiResult} from '../../Interface/ApiResult';
 
 @Component({
   selector: 'app-feeds',
@@ -8,10 +10,22 @@ import { LikeFeed, UploadFeed, FollowFeed } from './feeds';
   styleUrls: ['./feeds.component.less']
 })
 export class FeedsComponent implements OnInit {
-  feeds = FEEDS;
-  constructor() { }
+  feeds: (LikeFeed|FollowFeed|UploadFeed)[];
+  constructor(
+    private http: HttpService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(
+  ) {
+    this.get_feeds();
+  }
+  get_feeds() {
+    const callback = (result: ApiResult) => {
+      if (result.success) {
+        this.feeds = result.data.results;
+      }
+    }
+    this.http.get_all_my_feeds(callback);
   }
 
 }
